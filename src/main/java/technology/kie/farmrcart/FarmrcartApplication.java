92a012236845a0c89127d5bdcc187d45f8f23a59
harishbabusdc <harish.babu@sdc.company>
2019-12-23 09:49:24 +0530

    first commit

---
+++ src/main/java/technology/kie/farmrcart/FarmrcartApplication.java
@@ -0,0 +1,13 @@
+package technology.kie.farmrcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmrcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmrcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/technology/kie/farmrcart/FarmrcartApplicationTests.java
@@ -0,0 +1,13 @@
+package technology.kie.farmrcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmrcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
